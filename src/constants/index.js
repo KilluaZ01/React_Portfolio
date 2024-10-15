4dc31d55de8763cbdc1a0dd18616984295362b86
arik <raiarik97@gmail.com>
2024-10-15 08:55:45 +0545

    Logo Added in Navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Nayapati, Gokarneshwar, Kathmandu",
+  phoneNo: "+977 9848063688",
+  email: "raiarik97@gmail.com",
 };
