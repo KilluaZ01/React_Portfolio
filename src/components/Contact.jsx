a1c339f63ccd9ca6db438b260d64896b74458ed5
arik <raiarik97@gmail.com>
2024-10-19 22:53:16 +0545

    Added Socials to Footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,35 @@
 import { CONTACT } from "../constants";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <h1 className="my-20 text-center text-4xl">Get in Touch</h1>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/arik-rai-648b5b250"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/KilluaZ01"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/arik_raii/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </div>
       <div className="text-center tracking-tighter">
         <p className="my-4 ">{CONTACT.address}</p>
         <p className="my-4">{CONTACT.phoneNo}</p>
