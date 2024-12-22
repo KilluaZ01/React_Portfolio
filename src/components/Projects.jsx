a5f171cee9c2b39a1d4e1ec051b366d3fe2134df
arik <raiarik97@gmail.com>
2024-12-22 11:50:21 +0545

    Projects Optimized - Added Video for Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 
 const Projects = () => {
   const [hoveredProject, setHoveredProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const closeModal = () => setSelectedProject(null);
 
   return (
     <div className="border-b border-neutral-600 pb-4">
@@ -24,15 +27,26 @@ const Projects = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+              className="relative w-full lg:w-1/4"
             >
-              <img
-                className="mb-6 rounded"
-                src={project.image}
-                width={150}
-                height={150}
-                alt={project.title}
-              />
+              <div
+                className="relative group rounded overflow-hidden"
+                style={{ width: "150px", height: "150px" }}
+                onClick={() => setSelectedProject(project)}
+              >
+                <img
+                  className="block w-full h-full object-cover"
+                  src={project.image}
+                  width={150}
+                  height={150}
+                  alt={project.title}
+                />
+                <div className="absolute inset-0 flex items-center cursor-pointer justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <span className="text-white text-sm font-semibold">
+                    View Project
+                  </span>
+                </div>
+              </div>
             </motion.div>
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
@@ -58,7 +72,6 @@ const Projects = () => {
                   )}
                 </a>
               </div>
-
               <p className="mb-4 text-neutral-400">{project.description}</p>
               {project.technologies.map((tech, index) => (
                 <span
@@ -72,6 +85,79 @@ const Projects = () => {
           </div>
         ))}
       </div>
+
+      {/* Modal */}
+      {selectedProject && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+          onClick={closeModal}
+        >
+          <div
+            className="bg-neutral-800 rounded-lg p-8 w-11/12 max-w-5xl relative max-h-[90vh] overflow-y-auto"
+            onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+            style={{
+              scrollbarWidth: "none", // Hide scrollbar for Firefox
+              msOverflowStyle: "none", // Hide scrollbar for IE and Edge
+            }}
+          >
+            <button
+              className="absolute top-4 right-4 text-white hover:text-purple-800"
+              onClick={closeModal}
+            >
+              ✖
+            </button>
+            <h2 className="text-2xl mb-4 font-semibold text-white">
+              {selectedProject.title}
+            </h2>
+            <p className="text-neutral-400 mb-4">
+              {selectedProject.fullDescription
+                .split("\n")
+                .map((line, index) => (
+                  <span key={index}>
+                    {line}
+                    <br />
+                  </span>
+                ))}
+            </p>
+
+            <div className="mb-8">
+              {selectedProject.technologies.map((tech, index) => (
+                <span
+                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                  key={index}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            {selectedProject.demoVideo && (
+              <div className="mb-4">
+                <h3 className="text-lg font-medium text-white mb-2">
+                  Demo Video
+                </h3>
+                <video
+                  width="100%"
+                  height="auto"
+                  controls
+                  src={selectedProject.demoVideo}
+                  className="rounded-lg"
+                  alt={`${selectedProject.title} demo`}
+                />
+              </div>
+            )}
+
+            <a
+              href={selectedProject.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-purple-800 underline"
+            >
+              View Source Code on GitHub
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
 import projectBrickSync from "../assets/projects/project_BrickSync.png";
 import projectPersonalPortfolio from "../assets/projects/project_PersonalPortfolio.png";
+import BrickSyncDemo from "../assets/projects/BrickSyncDemo.mp4";
+import PortfolioDemo from "../assets/projects/PortfolioDemo.mp4";
 
 import certificateJsBasics from "../assets/certificates/certificate_jsBasics.jpg";
 import certificateJsIntermediate from "../assets/certificates/certificate_jsIntermediate.jpg";
@@ -8,7 +10,7 @@ import certificatePyBasics from "../assets/certificates/certificate_pyBasics.jpg
 export const HERO_CONTENT = `As a web developer specializing in the MERN stack, I focus on creating user-friendly, dynamic applications. Whether it's building a sleek front-end with React or ensuring smooth back-end functionality with Node.js and MongoDB, I strive to craft solutions that meet both business needs and user expectations.`;
 
 export const ABOUT_TEXT1 = `I'm Arik Rai, a web developer with a passion for creating interactive, efficient, and scalable web applications. My expertise lies in using the MERN stack (MongoDB, Express, React, Node.js) to build solutions that are not only visually appealing but also highly functional.`;
-export const ABOUT_TEXT2 = `I enjoy learning new technologies, solving problems, and continuously refining my skills. Outside of coding, I like exploring new tools and contributing to open-source projects`;
+export const ABOUT_TEXT2 = `I enjoy learning new technologies, solving problems, and continuously refining my skills. Outside of coding, I like exploring new tools and contributing to open-source projects.`;
 export const ABOUT_TEXT3 = `I’m always looking for exciting opportunities to collaborate on impactful projects and expand my knowledge.`;
 
 export const EXPERIENCES = [
@@ -16,7 +18,7 @@ export const EXPERIENCES = [
     year: "2022 - 2023",
     role: "Full Stack Developer",
     company: "Texas Imaginology",
-    description: `Led a team in developing a mobile application using Flutter, Node.js and MongoDB. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with students to define project requirements and timelines.`,
+    description: `Led a team in developing a mobile application using Flutter, Node.js, and MongoDB. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with students to define project requirements and timelines.`,
     technologies: ["Flutter", "Node.js", "MongoDB"],
   },
 ];
@@ -26,17 +28,23 @@ export const PROJECTS = [
     title: "BrickSync (Inventory & Expense Tracking App)",
     image: projectBrickSync,
     description:
-      "A fully functional full stack inventory & expense tracking webapp with features like product listing, inventory tracking, expense tracking, vehicle management and user authentication.",
+      "A fully functional full-stack inventory & expense tracking web app with features like product listing, inventory tracking, expense tracking, vehicle management, and user authentication.",
+    fullDescription:
+      "BrickSync is a comprehensive inventory management and expense tracking web application designed specifically for suppliers. It enables users to efficiently manage product stock by listing items, monitoring sales, and logging restocks. The app features low-stock alerts, ensuring timely replenishment of inventory, and provides detailed insights into revenue, expenses, and profit, offering a clear overview of financial performance.\n\nIn addition to inventory management, BrickSync helps track fuel expenses for owned vehicles, maintaining detailed histories of restocking, sales, and refueling activities. To support better decision-making, the app includes robust reporting tools, allowing users to generate charts and summaries of expenses and stock levels in PDF format. With its user-friendly interface and powerful functionality, BrickSync is an essential tool for streamlining supply chain operations and optimizing financial management for suppliers.",
     technologies: ["React", "Tailwind", "Node.js", "MongoDB"],
     githubLink: "https://github.com/KilluaZ01/BrickSync.git",
+    demoVideo: BrickSyncDemo,
   },
   {
     title: "Personal Portfolio",
     image: projectPersonalPortfolio,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
+    fullDescription:
+      "A personal portfolio website designed to showcase an individual's professional projects, skills, experience, and contact information. The website serves as a digital resume, highlighting the user's expertise and work across various fields such as web development, graphic design, content creation, or any other industry-specific skills. It features a clean, user-friendly layout with sections for presenting completed projects, providing details on technical and soft skills, and offering insight into personal achievements and professional background. The portfolio may also include client testimonials, downloadable resumes, and a contact form or social media links to make it easy for potential employers, collaborators, or clients to get in touch",
     technologies: ["React", "Tailwind"],
     githubLink: "https://github.com/KilluaZ01/React_Portfolio.git",
+    demoVideo: PortfolioDemo,
   },
 ];
 
